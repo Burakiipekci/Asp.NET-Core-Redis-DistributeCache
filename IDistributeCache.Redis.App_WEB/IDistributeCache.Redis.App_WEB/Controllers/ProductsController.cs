e3eb86d7902e310db4e37e777415a3ec1e207d01
Burak İpekci <burakipekci@outlook.com>
2023-08-10 14:36:55 +0300

    StackExchangeRedisCache Added

---
+++ IDistributeCache.Redis.App_WEB/IDistributeCache.Redis.App_WEB/Controllers/ProductsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace IDistributeCache.Redis.App_WEB.Controllers
+{
+    public class Products : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ IDistributeCache.Redis.App_WEB/IDistributeCache.Redis.App_WEB/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace IDistributeCache.Redis.App_WEB.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
